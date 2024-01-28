aeaef2f97b208dcf8ed7be7f54b53b5e3b05b58d
SHIRISH <shirish.shrestha07@gmail.com>
2024-01-28 23:29:48 +0545

    complete upto projects page and about me page, animation add in landing page, about and projects page

--- script.js
+++ script.js
@@ -1,21 +1,44 @@
+Shery.mouseFollower({
+  skew: true,
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
+Shery.makeMagnet(".logo a, .scroll", {
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
+
 var tl = gsap.timeline();
 
 gsap.to(".loader", {
   x: "-102%",
   // y: "100%",
-  duration: 2,
+  duration: 1.5,
   ease: "easeOut",
 });
 gsap.to(".loader__two", {
   x: "102%",
   // y: "-100%",
-  duration: 2,
+  duration: 1.5,
   ease: "easeOut",
+});
+
+tl.to(".hero__image, .hero__image img", {
+  // opacity: 0,
+  y: "55%",
+  x: "125%",
+  opacity: 0.8,
+  height: "2rem",
+  width: "2rem",
+  delay: 1.7,
+  duration: 1,
+  ease: "expo.out",
+  borderRadius: "50%",
 });
 
 tl.from(".hero__title h2:first-child", {
   x: -200,
-  delay: 1.8,
   opacity: 0,
   duration: 0.5,
   ease: "power1.inOut",
@@ -44,11 +67,22 @@ gsap.to(".scroll__icon", {
   repeat: -1,
 });
 
+gsap.from(".stripe > p", {
+  opacity: 0,
+  y: "-60%",
+  duration: 0.6,
+  stagger: 0.4,
+  scrollTrigger: {
+    trigger: ".about__desc",
+    start: "top 65%",
+  },
+});
+
 document.querySelectorAll(".stripe").forEach(function (stripe) {
   stripe.addEventListener("mousemove", function () {
     gsap.to(this.children[0], {
       height: "100%",
-      ease: "expo",
+      ease: "ease",
       duration: 0.5,
     });
 
@@ -61,11 +95,44 @@ document.querySelectorAll(".stripe").forEach(function (stripe) {
     gsap.to(this.children[0], {
       height: 0,
       ease: "expo",
-      duration: 0.5,
+      duration: 1,
     });
 
     gsap.to(this.children[1], {
       color: "#e9f1f3",
     });
   });
+});
+
+gsap.to(".desc__elem", {
+  scrollTrigger: {
+    trigger: ".project__content",
+    pin: true,
+    start: "top top",
+    end: "bottom bottom",
+    endTrigger: ".desc__last",
+    scrub: 1,
+  },
+  y: "-300%",
+  ease: "power1.in",
+});
+
+let description = document.querySelectorAll(".desc__elem");
+Shery.imageEffect(".pimages", {
+  style: 3,
+  config: { onMouse: { value: 1 } },
+  slideStyle: (setScroll) => {
+    description.forEach(function (description, index) {
+      ScrollTrigger.create({
+        trigger: description,
+        start: "50% 5%",
+        scrub: 1,
+        markers: true,
+        end: "110%",
+        onUpdate: function (prog) {
+          setScroll(prog.progress + index);
+        },
+      });
+    });
+  },
 });
