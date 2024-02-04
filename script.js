80dd109ff9cf5f006b0e7037815788c3e0f7db61
SHIRISH <shirish.shrestha07@gmail.com>
2024-02-04 20:11:19 +0545

    complete responsiveness upto 1024px

--- script.js
+++ script.js
@@ -1,47 +1,15 @@
-function handleSubmit(event) {
-  event.preventDefault();
-  const form = document.getElementById("contactForm");
-
-  const formData = new FormData(form);
-  const formDataObject = {};
-  formData.forEach((value, key) => {
-    formDataObject[key] = value;
+if (window.innewrwidth >= 1150) {
+  Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
   });
 
-  axios
-    .post(
-      "https://formsubmit.co/ajax/79770d6565ab4456c115c312d18b0761",
-      {
-        name: formDataObject.name,
-        email: formDataObject.email,
-        message: formDataObject.message,
-      },
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Accept: "application/json",
-        },
-      }
-    )
-    .then(() => {
-      form.style.display = "none";
-      const success = document.getElementById("successMessage");
-      success.style.display = "block";
-      form.reset();
-    })
-    .catch((error) => console.log(error));
+  Shery.makeMagnet(".logo a, .scroll, .project__github--button .button", {
+    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+    duration: 1,
+  });
 }
-
-Shery.mouseFollower({
-  skew: true,
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-});
-
-Shery.makeMagnet(".logo a, .scroll, .project__github--button .button", {
-  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-  duration: 1,
-});
 
 var tl = gsap.timeline();
 
@@ -65,7 +33,7 @@ tl.to(".hero__image, .hero__image img", {
   opacity: 0.8,
   height: "2rem",
   width: "2rem",
-  delay: 1.7,
+  delay: 1.4,
   duration: 1,
   ease: "expo.out",
   borderRadius: "50%",
@@ -157,61 +125,109 @@ splitTypes.forEach((char, i) => {
 //   });
 // });
 
-gsap.to(".desc__elem", {
-  scrollTrigger: {
-    trigger: ".project__content",
-    pin: true,
-    start: "top top",
-    end: "bottom bottom",
-    endTrigger: ".desc__last",
-    scrub: 1,
-  },
-  y: "-300%",
-  ease: "power1.in",
-});
-
-let description = document.querySelectorAll(".desc__elem");
-Shery.imageEffect(".pimages", {
-  style: 4,
-  config: { onMouse: { value: 1 } },
-  slideStyle: (setScroll) => {
-    description.forEach(function (description, index) {
-      ScrollTrigger.create({
-        trigger: description,
-        start: "50% 5%",
+const changeDesign = () => {
+  if (window.innerWidth >= 1100) {
+    gsap.to(".desc__elem", {
+      scrollTrigger: {
+        trigger: ".project__content",
+        pin: true,
+        start: "top top",
+        end: "bottom bottom",
+        endTrigger: ".desc__last",
         scrub: 1,
-        end: "110%",
-        onUpdate: function (prog) {
-          setScroll(prog.progress + index);
-        },
-      });
+      },
+      y: "-300%",
+      ease: "power1.in",
     });
-  },
-});
+    let description = document.querySelectorAll(".desc__elem");
+    Shery.imageEffect(".pimages", {
+      style: 4,
+      config: { onMouse: { value: 1 } },
+      slideStyle: (setScroll) => {
+        description.forEach(function (description, index) {
+          ScrollTrigger.create({
+            trigger: description,
+            start: "50% 5%",
+            scrub: 1,
+            end: "110%",
+            onUpdate: function (prog) {
+              setScroll(prog.progress + index);
+            },
+          });
+        });
+      },
+    });
+  } else if (window.innerWidth < 1100) {
+    let images = document.querySelector(".project__images");
+    images.style.display = "none";
+  }
+};
+changeDesign();
 
-gsap.from(".tech__stack--left .stack__images", {
-  x: "100%",
-  opacity: 0,
-  duration: 1.5,
-  stagger: 0.5,
-  scrollTrigger: {
-    trigger: ".project__github--button",
-    endTrigger: ".tech__third",
-    scrub: 3,
-    start: "top 15%",
-    end: "bottom 60%",
-  },
-});
-gsap.from(".tech__stack--right .stack__images", {
-  x: "-100%",
-  opacity: 0,
-  duration: 1.5,
-  stagger: 0.5,
-  scrollTrigger: {
-    trigger: ".project__github--button",
-    endTrigger: ".tech__third",
-    scrub: 3,
-    start: "top 15%",
-    end: "bottom 60%",
-  },
-});
+if (window.innerWidth >= 1150) {
+  gsap.from(".tech__stack--left .stack__images", {
+    x: "100%",
+    opacity: 0,
+    duration: 1.5,
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: ".project__github--button",
+      endTrigger: ".tech__third",
+      scrub: 3,
+      start: "top 15%",
+      end: "bottom 60%",
+    },
+  });
+  gsap.from(".tech__stack--right .stack__images", {
+    x: "-100%",
+    opacity: 0,
+    duration: 1.5,
+    stagger: 0.5,
+    scrollTrigger: {
+      trigger: ".project__github--button",
+      endTrigger: ".tech__third",
+      scrub: 3,
+      start: "top 15%",
+      end: "bottom 60%",
+    },
+  });
+}
+
+const year = new Date().getFullYear();
+
+const displayYear = document.querySelector(".copyright span");
+displayYear.innerHTML = year;
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = document.getElementById("contactForm");
+
+  const formData = new FormData(form);
+  const formDataObject = {};
+  formData.forEach((value, key) => {
+    formDataObject[key] = value;
+  });
+
+  axios
+    .post(
+      "https://formsubmit.co/ajax/79770d6565ab4456c115c312d18b0761",
+      {
+        name: formDataObject.name,
+        email: formDataObject.email,
+        message: formDataObject.message,
+      },
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      }
+    )
+    .then(() => {
+      form.style.display = "none";
+      const success = document.getElementById("successMessage");
+      success.style.display = "block";
+      form.reset();
+    })
+    .catch((error) => console.log(error));
+}
